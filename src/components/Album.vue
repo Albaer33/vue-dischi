c1e594695b68a686ba6129de6d95e6599f086002
Albaer33 <alisi3396@gmail.com>
2021-12-22 15:43:02 +0100

    single album data structure done

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,12 +1,21 @@
 <template>
-    <div></div>
+    <div class="album">
+        <div class="album-img">
+            <img :src="albumObj.poster" :alt="albumObj.author">
+        </div>
+        <div class="album-text">
+            <div class="title">{{albumObj.title}}</div>
+            <div class="author">{{albumObj.author}}</div>
+            <div class="year">{{albumObj.year}}</div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Album',
     props: {
-
+        albumObj: Object
     }
 }
 </script>
